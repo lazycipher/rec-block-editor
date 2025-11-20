5146e86e310f746ce16dbfd3d91082d657ac62ba
Himanshu Singh <imhsingh16@gmail.com>
2025-11-20 18:49:31 +0530

    GH Pages url fix

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? "/recon-editor/" : "/";
+const repoBase = "/recon-editor/";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: basePath,
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? repoBase : "/",
   plugins: [react(), tailwindcss()],
-});
+}));
