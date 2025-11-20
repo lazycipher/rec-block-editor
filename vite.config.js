0d12a12d587ffad3cf220dc51855ca00f1d5b902
Himanshu Singh <imhsingh16@gmail.com>
2025-11-20 18:29:26 +0530

    adds deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
+const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? "/recon-editor/" : "/";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: basePath,
   plugins: [react(), tailwindcss()],
 });
