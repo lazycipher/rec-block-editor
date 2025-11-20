d0945b86374c8bf87e8d5270e21c0cc642d57f08
Himanshu Singh <imhsingh16@gmail.com>
2025-11-20 18:53:12 +0530

    base setup for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-const repoBase = "/recon-editor/";
-
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? repoBase : "/",
+export default defineConfig({
   plugins: [react(), tailwindcss()],
-}));
+  base: "/rec-block-editor/",
+});
